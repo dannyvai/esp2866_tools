1d1069e93ef8e02dc7698990a854775c8211cc1d
DW <danny.wainshtien@gmail.com>
2015-01-09 19:16:36 +0200

    added a server that serves as a hotspot and after connecting and writing the ssid,pass it connects to the real server for now a file named connect.lua

--- nodemcu/upload_file.py
+++ nodemcu/upload_file.py
@@ -11,24 +11,26 @@ if os.path.exists(sys.argv[2]) is False:
     exit(1)
 
 ser = serial.Serial(sys.argv[1],baudrate=9600)
-ser.timeout = 0.01
+ser.timeout = 0.1
 upload_file = open(sys.argv[2],'r')
 
 
 lines = upload_file.readlines()
 for line in lines:
-    print "writing to esp : %s " %  line
+    #print "writing to esp : %s " %  line
     ser.write(chr(10))
     ser.write(line + chr(10))
+
     res = ser.read(1024)
-    while len(res) == 0:
+    while line.strip() not in res:
         res = ser.read(1024)
+    
     print "got from esp : %s " % res
 
 ser.timeout = 1
 res = ""
-while len(res) == 0:
-        res = ser.read(1024)
+while len(res) > 0:
+    res = ser.read(1024)
 print "got from esp : %s " % res
 
 upload_file.close()
